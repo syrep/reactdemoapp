2faa5a2875636c2491300aad36ed821d96bbb630
syrep <my.infinitel00p8@gmail.com>
2020-12-06 13:32:47 +1100

    Hooks Components Added

--- src/App.js
+++ src/App.js
@@ -4,55 +4,56 @@ import Users from "./AxiosPart/Users";
 import UsersModify from "./AxiosPart/UsersModify";
 import { Hello, Hello2, Hello3, Hello4, Hello5 } from "./FunctionalPart/Hello";
 import DynUser from "./DynamicPart/Dynamic";
-import Hooks from "./HooksPart/Hooks";
+import { HooksFunc, HookComp } from "./HooksPart/Hooks";
 
 function App() {
-	return (
-		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>
-					New <code>/.code-test</code> with:
-				</p>
-				<ul>
-					<li>REST API calls using Axios</li>
-					<li>Functional Components</li>
-					<li>Dynamic Functions</li>
-					<li>x React Hooks</li>
-					<li>x Plotly.js</li>
-					<li>x Redux</li>
-				</ul>
-			</header>
-			<div className="subheader">
-				<div>
-					<h2>Axios GET example</h2>
-					<Users />
-				</div>
-				<div>
-					<h2>Axios POST example</h2>
-					<UsersModify />
-				</div>
-				<div>
-					<h2>Functional Component and props example</h2>
-					<Hello />
-					<Hello2 age="6" />{" "}
-					{/* Or can use {} for actual numerical operations */}
-					<Hello3 name="NAME" />
-					<Hello4 class="classname" />
-					<Hello5 work="work" />
-				</div>
-				<div>
-					<br />
-					<h2>Dynamic Functions and state example</h2>
-					<DynUser />
-				</div>
-				<div>
-					<br />
-					<h2>Hooks example</h2>
-					<Hooks />
-				</div>
-			</div>
-		</div>
-	);
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          New <code>/.code-test</code> with:
+        </p>
+        <ul>
+          <li>REST API calls using Axios</li>
+          <li>Functional Components</li>
+          <li>Dynamic Functions</li>
+          <li>React Hooks</li>
+          <li>x Plotly.js</li>
+          <li>x Redux</li>
+        </ul>
+      </header>
+      <div className="subheader">
+        <div>
+          <h2>Axios GET example</h2>
+          <Users />
+        </div>
+        <div>
+          <h2>Axios POST example</h2>
+          <UsersModify />
+        </div>
+        <div>
+          <h2>Functional Component and props example</h2>
+          <Hello />
+          <Hello2 age="6" />{" "}
+          {/* Or can use {} for actual numerical operations */}
+          <Hello3 name="NAME" />
+          <Hello4 class="classname" />
+          <Hello5 work="work" />
+        </div>
+        <div>
+          <br />
+          <h2>Dynamic Functions and state example</h2>
+          <DynUser />
+        </div>
+        <div>
+          <br />
+          <h2>Hooks example</h2>
+          <HooksFunc />
+          <HookComp />
+        </div>
+      </div>
+    </div>
+  );
 }
 export default App;
--- src/HooksPart/Hooks.js
+++ src/HooksPart/Hooks.js
@@ -3,15 +3,15 @@ import "./Hooks.css";
 
 //first hook will be the mapping one, then need one that can be used as a boolean
 // https://www.youtube.com/watch?v=mxK8b99iJTg
-//for the first hook
+
 // https://www.youtube.com/watch?v=O6P86uwfdR0
-//for the second hook
+//for the hooks that are simple arrays
 
 //need a function component to use hook, classes use "state" instead
 //hooks need to execute in the same order, so we can't use if statements for them
 //usually we use a single hook per object in an array, but it really just depends on what you're doing
 // single hooks then let you do various different changes on a single/multiple event/s, making the data truely uniquely modifiable
-export default function Hooks() {
+export function HooksFunc() {
   const [countState, setCountState] = useState(() => {
     return 0;
   });
@@ -65,17 +65,126 @@ export default function Hooks() {
       <button onClick={increaseCount}>Up</button>
       <span className="counter">{countState}</span>
       <button onClick={decreaseCount}>Down</button>
-      <div>
-        <p>
-          {number}, {letter}
-        </p>
+      <div className="spacer">
         <button onClick={increaseNumber}>Up Number</button>
+        <span className="counter">
+          {number}, {letter}
+        </span>
         <button onClick={decreaseNumber}>Down Number</button>
       </div>
     </div>
   );
 }
 
-function HookComp({ hook, index }) {
-  return <div className="hello">{hook.text}</div>;
+/// the below is where the modifiable useState stuff is done. It is independant of the stuff above
+/// this is really annoying to figure out
+/// it has some changes and explanations that were not in the videos, so it was a good learning exercise
+
+function ListItem({ item, index, modifyArray, deleteArray }) {
+  return (
+    <li
+      style={{ textDecoration: item.isModified ? "line-through" : "" }}
+      className="list-item"
+    >
+      {item.text}
+      <button onClick={() => modifyArray(index)}>Modify</button>
+      <button onClick={() => deleteArray(index)}>Delete</button>
+    </li>
+  );
+  //text decoration is based on a ternary? statement, basically the syntax is "if XYZ ? true=action : else=action"
+  // in this specific case
+}
+
+function AddListItem({ addItem }) {
+  const [newItem, setNewItem] = useState("");
+
+  const handleAdd = (e) => {
+    e.preventDefault();
+    if (!newItem) return;
+    addItem(newItem);
+    setNewItem("");
+  };
+
+  return (
+    <form onSubmit={handleAdd}>
+      <input
+        type="text"
+        value={newItem}
+        placeholder="add new item"
+        onChange={(e) => setNewItem(e.target.value)}
+      ></input>
+    </form>
+  );
+}
+
+export function HookComp() {
+  const [arrayList, setArrayList] = useState([
+    {
+      text: "first item",
+      isModified: false,
+    },
+    {
+      text: "second item",
+      isModified: true,
+    },
+    {
+      text: "third item",
+      isModified: false,
+    },
+  ]);
+
+  const addItem = (text) => {
+    const newValues = [...arrayList, { text }];
+    setArrayList(newValues);
+  };
+
+  const modifyArray = (index) => {
+    const newValues = [...arrayList];
+    //newValues[index].isModified = true;
+    //setArrayList(newValues);
+
+    //the commented out way would only work for strike-through, but the way below allows for undoing the change
+    if (newValues[index].isModified) {
+      return (newValues[index].isModified = false), setArrayList(newValues);
+    }
+    {
+      return (newValues[index].isModified = true), setArrayList(newValues);
+    }
+    //this if statement works by assessing if the isModified == true, then sets it to false, else it sets it true
+    //we need to call the setState function within the block otherwise it can't access the changed value
+  };
+
+  const deleteArray = (index) => {
+    ///const newValues = [...arrayList];
+    ///newValues.splice(index, 1);
+    //puts "cursor" at index, then splices 1 position. Look it up for more information if needed.
+
+    ///setArrayList(newValues);
+    // the triple slash comments are what was previously there, but the new filter method does not mutate the array
+
+    const deletedItem = Array.from(arrayList.filter((items, i) => i !== index));
+    //the filter can take in the callback function with the array (items) and the index (i), then we
+    // are checking if the index does not equal the index of the item we are deleting, so we can exclude
+    // it from the new array. After that, we set state equal to this new array (non-mutated)
+    setArrayList(deletedItem);
+  };
+
+  return (
+    <div className="comp-container">
+      <div className="array-list">
+        <ul>
+          {arrayList.map((item, index) => (
+            <ListItem
+              key={index}
+              index={index}
+              item={item}
+              modifyArray={modifyArray}
+              deleteArray={deleteArray}
+            />
+          ))}
+          <AddListItem addItem={addItem} />
+        </ul>
+      </div>
+    </div>
+  );
 }
