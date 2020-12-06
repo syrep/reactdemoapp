024ec4c9668278145ba87e3b1ad445dc736e7065
syrep <my.infinitel00p8@gmail.com>
2020-12-06 17:22:28 +1100

    fixed the import of provider

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import reportWebVitals from "./reportWebVitals";
 
 import { createStore } from "redux";
 import allReducers from "./reduxpart/reducers/combinedReducers";
-import Provider from "react-redux";
+import { Provider } from "react-redux";
 
 const store = createStore(allReducers);
 //putting the store outside so when the actual file is needed, we can access it
