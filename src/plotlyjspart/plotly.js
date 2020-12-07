6c821de2c5efb1a931746e023c4f0eab1e8d8314
syrep <my.infinitel00p8@gmail.com>
2020-12-07 17:04:37 +1100

    basic plotly.js graph implemented

--- src/plotlyjspart/plotly.js
+++ src/plotlyjspart/plotly.js
@@ -1,7 +1,29 @@
-import React from "react";
+import Plot from "react-plotly.js";
 
-export default class PlotlyJSPart extends React.Component {
-  render() {
-    return <p>Plotly Helllo</p>;
-  }
+export default function PlotlyJSPart() {
+  var tracecohort = {
+    y: ["S1", "S2", "S3"],
+    x: [20, 14, 23],
+    name: "Cohort Average",
+    type: "bar",
+    orientation: "h",
+  };
+
+  var traceuser = {
+    y: ["S1", "S2", "S3"],
+    x: [12, 18, 29],
+    name: "Individual Average",
+    type: "bar",
+    orientation: "h",
+  };
+
+  var data = [traceuser, tracecohort];
+
+  var layout = {
+    barmode: "group",
+    legend: { orientation: "h" },
+    title: "Average Scores",
+  };
+
+  return <div>{<Plot data={data} layout={layout} />}</div>;
 }
